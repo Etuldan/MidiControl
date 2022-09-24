84248bba40aac483be2da6f2ccbcbb421f909bfc
Jace Boby <jboby93@gmail.com>
2022-09-23 23:38:05 -0400

    update copyright year

--- MidiControl/Properties/AssemblyInfo.cs
+++ MidiControl/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Etuldan")]
 [assembly: AssemblyProduct("MIDIControl")]
-[assembly: AssemblyCopyright("Copyright © 2020-2021")]
+[assembly: AssemblyCopyright("Copyright © 2020-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
