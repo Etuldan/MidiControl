453af01536988ffec9aaec6bf19424ce415644d2
Jace Boby <jboby93@gmail.com>
2022-12-03 01:18:01 -0500

    fix summarize crash when obs callback action arguments are empty

--- MidiControl/Configuration.cs
+++ MidiControl/Configuration.cs
@@ -191,23 +191,32 @@ namespace MidiControl
 			if(OBSCallBacksON.Count > 0) {
 				var items = new List<string>();
 				foreach(var item in OBSCallBacksON) {
-					items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
-				}
+                    if(item.Args != null)
+                        items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
+                    else
+                        items.Add(item.Action);
+                }
 				summary.Add("OBS (on): " + string.Join("; ", items));
 				
 			}
 			if(OBSCallBacksOFF.Count > 0) {
 				var items = new List<string>();
 				foreach(var item in OBSCallBacksOFF) {
-					items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
-				}
+                    if(item.Args != null)
+                        items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
+                    else
+                        items.Add(item.Action);
+                }
 				summary.Add("OBS (off): " + string.Join("; ", items));
 			}
 			if(OBSCallBacksSlider.Count > 0) {
 				var items = new List<string>();
 				foreach(var item in OBSCallBacksSlider) {
-					items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
-				}
+                    if(item.Args != null)
+                        items.Add(item.Action + ": " + string.Join(", ", item.Args.ToArray()));
+                    else
+                        items.Add(item.Action);
+                }
 				summary.Add("(adjust) " + string.Join("; ", items));
 			}
 			if(SoundCallBack != null) {
