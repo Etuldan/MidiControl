430a236d27d67fd2dd3ffaa20b9dbae9c0c75ee2
Etuldan <arnaud@renaud-goud.fr>
2023-02-18 11:40:16 +0100

    version 2.0.0.3

--- MidiControl/Properties/AssemblyInfo.cs
+++ MidiControl/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("3fcea34e-a5c1-4034-a7c9-df52ca1112e7")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.3")]
