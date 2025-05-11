46ad0d428f4c00cc008448f502237e308dbe27e8
Arnaud RENAUD-GOUD <arnaud@renaud-goud.fr>
2025-05-11 18:45:27 +0200

    new obs implementation

--- internal/connector/obs.go
+++ internal/connector/obs.go
@@ -5,12 +5,21 @@ import (
 	"midicontrol/internal/tools"
 
 	"github.com/andreykaipov/goobs"
+	"github.com/andreykaipov/goobs/api/requests/inputs"
+	"github.com/andreykaipov/goobs/api/requests/sceneitems"
 	"github.com/andreykaipov/goobs/api/requests/scenes"
+	"github.com/andreykaipov/goobs/api/requests/sources"
 )
 
 const (
 	OBS_SWITCHSCENE  = "switchScene"
 	OBS_PREVIEWSCENE = "previewScene"
+	OBS_MUTE         = "mute"
+	OBS_UNMUTE       = "unmute"
+	OBS_TOGGLEMUTE   = "togglemute"
+	OBS_HIDE         = "hide"
+	OBS_SHOW         = "show"
+	OBS_TOGGLEHIDE   = "togglehide"
 )
 
 type Obs struct {
@@ -66,15 +75,85 @@ func (k Obs) doAction(action string, params ...string) error {
 	var err error
 	switch action {
 	case OBS_SWITCHSCENE:
-		scene := &scenes.SetCurrentProgramSceneParams{
+		_, err = k.o.Scenes.SetCurrentProgramScene(&scenes.SetCurrentProgramSceneParams{
 			SceneName: &params[0],
-		}
-		_, err = k.o.Scenes.SetCurrentProgramScene(scene)
+		})
 	case OBS_PREVIEWSCENE:
-		scene := &scenes.SetCurrentPreviewSceneParams{
+		_, err = k.o.Scenes.SetCurrentPreviewScene(&scenes.SetCurrentPreviewSceneParams{
 			SceneName: &params[0],
+		})
+	case OBS_MUTE:
+		mute := true
+		for _, source := range params {
+			k.o.Inputs.SetInputMute(&inputs.SetInputMuteParams{
+				InputMuted: &mute,
+				InputName:  &source,
+			})
 		}
-		_, err = k.o.Scenes.SetCurrentPreviewScene(scene)
+	case OBS_UNMUTE:
+		mute := false
+		for _, source := range params {
+			k.o.Inputs.SetInputMute(&inputs.SetInputMuteParams{
+				InputMuted: &mute,
+				InputName:  &source,
+			})
+		}
+	case OBS_TOGGLEMUTE:
+		for _, source := range params {
+			resp, err := k.o.Inputs.GetInputMute(&inputs.GetInputMuteParams{
+				InputName: &source,
+			})
+			mute := !resp.InputMuted
+			if err == nil {
+				k.o.Inputs.SetInputMute(&inputs.SetInputMuteParams{
+					InputMuted: &mute,
+					InputName:  &source,
+				})
+			}
+		}
+	case OBS_HIDE:
+		hide := true
+		return k.manageScenes(&hide)
+	case OBS_SHOW:
+		hide := false
+		return k.manageScenes(&hide)
+	case OBS_TOGGLEHIDE:
+		return k.manageScenes(nil)
 	}
 	return err
+}
+
+func (k Obs) manageScenes(hideParam *bool) error {
+	scenes, err := k.o.Scenes.GetSceneList()
+	if err != nil {
+		return err
+	}
+	for _, scene := range scenes.Scenes {
+		var hide bool
+		if hideParam == nil {
+			resp, err := k.o.Sources.GetSourceActive(&sources.GetSourceActiveParams{
+				SourceName: &scene.SceneName,
+			})
+			if err != nil {
+				return err
+			}
+			hide = !resp.VideoShowing
+		} else {
+			hide = *hideParam
+		}
+
+		sceneItemList, err := k.o.SceneItems.GetGroupSceneItemList(&sceneitems.GetGroupSceneItemListParams{
+			SceneName: &scene.SceneName,
+		})
+		if err == nil {
+			for _, item := range sceneItemList.SceneItems {
+				k.o.SceneItems.SetSceneItemEnabled(&sceneitems.SetSceneItemEnabledParams{
+					SceneItemEnabled: &hide,
+					SceneItemId:      &item.SceneItemID,
+					SceneName:        &scene.SceneName,
+				})
+			}
+		}
+	}
+	return nil
 }
